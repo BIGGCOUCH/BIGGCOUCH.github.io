327fd5c3a148b674c7e412605d62ded051fbc00f
BIGGCOUCH <wlcouch1@gmail.com>
2025-04-11 20:56:50 -0400

    Java Frontend

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub to fetch grade data from the database (future implementation)
+function fetchGradeData() {
+    console.log("fetchGradeData called");
+    // Placeholder: This function will fetch data in a future assignment
+}
+
+// Stub to populate the gradebook with fetched data
+function populateGradebook(data) {
+    console.log("populateGradebook called with data:", data);
+    // Placeholder: This function will insert rows into the table in the future
+}
+
+// Call the stubs to simulate usage
+fetchGradeData();
+populateGradebook([]);
